fddb5444b846f1e4feeeb75d31efd4391137e20f
Anthony <relampago1986@hotmail.com>
2015-06-22 00:16:42 -0400

    first commit

---
+++ cheatcheet.js
@@ -0,0 +1,40 @@
+//How to select elements of a page in the JS console
+
+$("body")
+$(".wrapper")
+
+//How to make sure your JS doesn't start running before all elements have loaded
+
+$(document).ready( function() {
+	//JS code goes here
+})
+
+//How to show and hide elements of the page
+
+$("body").show();
+$("body").hide();
+
+//How to make elements fade in or fade out
+
+$("body").fadeIn();
+$("body").fadeOut(4000);  //this will take 4 seconds
+
+//How to make elements slide up
+//You can also put the time
+
+$("body").slideUp(1000);
+$("body").slideDown();
+
+//How to animate your CSS properties
+//Indicate time in milliseconds
+
+$("body").animate({
+	opacity: 0.25,
+	width: 70%
+}, 2000 );
+
+//How to make something happen when an element is clicked
+$("body").click( function(){
+	//JS code goes here
+	alert("You clicked!");
+})
---
+++ jquery.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	$("h1").click(function(){
+	 $("h1").animate({right:'800px'},"slow");
+	
+});
+
+$("#parrafo1").click(function(){
+    $("div").slideUp(1000);
+ 
+
+ });
+
+ $("#nav").click(function(){
+    alert("Clickeaste");
+ });
+  $("#parrafo2").click(function(){
+  		 	$("#nav2").hide();
+   	
+    	});
+
+
+});
+
